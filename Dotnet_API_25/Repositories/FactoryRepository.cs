2cb1b951a3b1d4969ce1bfe3760f949707e2ade1
tapan0810 <arceus0810@gmail.com>
2026-03-14 15:29:13 +0530

    feat : Implemented Repositories Pattern : DbContext -> Repositories ->Services

---
+++ Dotnet_API_25/Repositories/FactoryRepository.cs
@@ -0,0 +1,42 @@
+﻿using Dotnet_API_25.Data;
+using Dotnet_API_25.Entities.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Dotnet_API_25.Repositories
+{
+    public class FactoryRepository(FactoryDbContext _context) : IFactoryRepository
+    {
+        public async Task<Factory> CreateFactory(Factory factory)
+        {
+            _context.Factories.Add(factory);
+            await _context.SaveChangesAsync();
+            return factory;
+        }
+
+        public async Task DeleteFactory(int id)
+        {
+            var factory = await _context.Factories.FindAsync( id);
+            if(factory != null)
+            {
+                 _context.Factories.Remove(factory);
+                await _context.SaveChangesAsync();  
+            }
+        }
+
+        public async Task<IEnumerable<Factory>> GetAllFactory()
+        {
+            return await _context.Factories.ToListAsync();
+        }
+
+        public Task<Factory?> GetFactoryById(int id)
+        {
+            return _context.Factories.FirstOrDefaultAsync( x =>x.Id == id);
+        }
+
+        public async Task UpdateFactory(Factory updateFactory)
+        {
+            _context.Factories.Update(updateFactory);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
---
+++ Dotnet_API_25/Repositories/IFactoryRepository.cs
@@ -0,0 +1,17 @@
+﻿using Dotnet_API_25.Entities.DTOs;
+using Dotnet_API_25.Entities.Models;
+
+namespace Dotnet_API_25.Repositories
+{
+    public interface IFactoryRepository
+    {
+        public Task<IEnumerable<Factory>> GetAllFactory();
+        public Task<Factory?> GetFactoryById(int id);
+
+        public Task<Factory> CreateFactory(Factory factory);
+        public Task UpdateFactory(Factory updateFactory);
+        public Task DeleteFactory(int id);
+
+
+    }
+}
