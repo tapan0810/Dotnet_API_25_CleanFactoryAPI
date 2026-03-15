e28c4e7be8abbec368e80671545dcd4c758b6329
tapan0810 <arceus0810@gmail.com>
2026-03-15 12:41:49 +0530

    feat : Impelmented User AutoMapper

---
+++ Dotnet_API_25/Mapping/UserMappingProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using Dotnet_API_25.Entities.DTOs.UserDto;
+using Dotnet_API_25.Entities.Models;
+
+namespace Dotnet_API_25.Mapping
+{
+    public class UserMappingProfile:Profile
+    {
+        public UserMappingProfile() 
+        {
+            CreateMap<RegisterDto, User>();
+
+            CreateMap<User, LoginDto>();
+        }
+    }
+}
